00df2166ed5112caad3f8e6224c665bb2252a2fc
Charles McKelvey <chazmckelvey@gmail.com>
2019-09-28 21:18:02 -0400

    Trying to add an attack but still is not appearing on either side. Supposed to simulate a spear attack when moving in a specific direction

--- player.js
+++ player.js
@@ -1,46 +1,72 @@
 class Player {
-    constructor(game, gameSize) {
-        this.game = game
-        console.log(this.game)
-        this.center = { x: gameSize.x / 2, y: gameSize.y / 2 } // This will be updated as keys are pressed
-        console.log(this.center)
-        this.size = { x: 15, y: 15 } // Constant dependent upon character type: base character can be 15x15. Warrior 20x20, Mage/Priest base
-        
-        this.keyboarder = new Keyboarder()
-        this.color = colors.player
-        window.playerPosition = this.center
-    }
+	constructor(game, gameSize) {
+		this.game = game;
+		this.position = { x: gameSize.x / 2, y: gameSize.y / 2 }; // This will be updated as keys are pressed
+		this.size = { x: 15, y: 15 }; // Constant dependent upon character type: base character can be 15x15. Warrior 20x20, Mage/Priest base
+		this.keyboarder = new Keyboarder();
+		this.color = colors.player;
 
-    attack() {
-        // This will create a rect in at the position the player is aiming. 
-        // Because size is at 15x15, the rect will be 20 width, 10 height when swung down and up, 
-        // and 10 width and 20 height when swung left or right.
-    }
+		window.playerPosition = this.position;
 
-    aim(direction) {
-        // This will determine the direction that the player is facing.
-        // This is used in tandem with attack()
-    }
+		this.collide = true;
+		this.alive = true;
+	}
 
-    update() {
-        // Aiming will be apart of the basic attack. You will swing in that direction, creating an invisible rectangle of attack  
-        console.log(this.keyboarder)
-        if (this.keyboarder.isDown(Keyboarder.KEYS.MOVELEFT)) { // A
-            this.center.x -= 2
-        } else if (this.keyboarder.isDown(Keyboarder.KEYS.MOVERIGHT)) { // D
-            this.center.x += 2
-        } else if (this.keyboarder.isDown(Keyboarder.KEYS.MOVEUP)) { // W
-            this.center.y -= 2
-        } else if (this.keyboarder.isDown(Keyboarder.KEYS.MOVEDOWN)) { // S
-            this.center.y += 2
-        }
+	attack(direction) {
+		if (this.keyboarder.isDown(Keyboarder.KEYS.ATTACK)) {
+			if (direction === 'UP') {
+				// Positioning and sizing and then creating and adding the Attack to the bodies
+				// Supposed to simulate a spear attack.
+				let attackPosition = { x: this.size.x - 8, y: this.size.y - 28 };
+				let attackSize = { width: 15, height: 20 };
+				let attack = new Attack(this.game, attackPosition, attackSize);
+				this.game.addBody(attack);
+			} else if (direction === 'RIGHT') {
+				let attackPosition = { x: this.size.x + 8, y: this.size.y - 7.5 };
+				let attackSize = { x: 20, y: 15 };
+				let attack = new Attack(this.game, attackPosition, attackSize);
+				this.game.addBody(attack);
+			} else if (direction === 'DOWN') {
+				let attackPosition = { x: this.size.x - 7.5, y: this.size.y - 8 };
+				let attackSize = { x: 15, y: 20 };
+				let attack = new Attack(this.game, attackPosition, attackSize);
+				this.game.addBody(attack);
+			} else if (direction === 'LEFT') {
+				let attackPosition = { x: this.size.x - 28, y: this.size.y - 7.5 };
+				let attackSize = { x: 20, y: 15 };
+				let attack = new Attack(this.game, attackPosition, attackSize);
+				this.game.addBody(attack);
+			}
+		}
+	}
 
-        // Add later for attack animation 
-
-        // if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
-        //     this.attack() // Still needs to be made 
-        // }
-    }
-
-
+	update() {
+		let direction = 'DOWN';
+		// Aiming will be apart of the basic attack. You will swing in that direction, creating an invisible rectangle of attack
+		console.log(this.keyboarder);
+		if (this.keyboarder.isDown(Keyboarder.KEYS.MOVELEFT)) {
+			// A
+			this.position.x -= 1.5;
+			direction = 'LEFT';
+			this.attack(direction);
+		}
+		if (this.keyboarder.isDown(Keyboarder.KEYS.MOVERIGHT)) {
+			// D
+			this.position.x += 1.5;
+			direction = 'RIGHT';
+			this.attack(direction);
+		}
+		if (this.keyboarder.isDown(Keyboarder.KEYS.MOVEUP)) {
+			// W
+			this.position.y -= 1.5;
+			direction = 'UP';
+			this.attack(direction);
+		}
+		if (this.keyboarder.isDown(Keyboarder.KEYS.MOVEDOWN)) {
+			// S
+			this.position.y += 1.5;
+			direction = 'DOWN';
+			this.attack(direction);
+		}
+	}
 }
