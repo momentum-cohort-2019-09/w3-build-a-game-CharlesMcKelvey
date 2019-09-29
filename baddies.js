2e07ae7d337e3f212aeafcfef15ed58e0c4fc364
Charles McKelvey <chazmckelvey@gmail.com>
2019-09-28 21:16:54 -0400

    Made them spawn at the sides and gave them movement specifically towards me

--- baddies.js
+++ baddies.js
@@ -1,18 +1,32 @@
 class Baddies {
-    constructor (game, position) {
-        this.game = game
-        this.position = {x: position.x, y: position.y}
-        this.size = {x: 15, y: 15}
-    }
+	constructor(game, position) {
+		this.game = game;
+		this.position = { x: position.x, y: position.y };
+		this.size = { x: 15, y: 15 };
+		this.color = colors.baddies;
+		this.speed = 1;
+		this.angularVelocity = this.aim(window.playerPosition);
 
-    draw(size) {
-        screen.clearRect(0, 0, size.x, size.y)
-        for (let body of this.bodies) {
-            this.render(screen, body)
-        }
-    }
+		this.collide = false;
+		this.alive = true;
+	}
 
-    update() {
-        
-    }
+	draw(size) {
+		screen.clearRect(0, 0, size.x, size.y);
+		for (let body of this.bodies) {
+			this.render(screen, body);
+		}
+	}
+	aim(player) {
+		let theta = Math.atan2(player.y - this.position.y, player.x - this.position.x);
+		let xSpeed = this.speed * Math.cos(theta);
+		let ySpeed = this.speed * Math.sin(theta);
+
+		return { x: xSpeed, y: ySpeed };
+	}
+	update() {
+		// Add the colision detection
+		this.position = { x: this.position.x + this.angularVelocity.x, y: this.position.y + this.angularVelocity.y };
+		this.angularVelocity = this.aim(window.playerPosition);
+	}
 }
