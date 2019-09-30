04563d4f474b5ad5587c7d4c7676cb9483c2b562
Charles McKelvey <chazmckelvey@gmail.com>
2019-09-29 21:22:48 -0400

    Mostly just removing console logs as well as making them all random colors with a randomColors function in the game class

--- baddies.js
+++ baddies.js
@@ -2,8 +2,8 @@ class Baddies {
 	constructor(game, position) {
 		this.game = game;
 		this.position = { x: position.x, y: position.y };
-		this.size = { x: 15, y: 15 };
-		this.color = colors.baddies;
+		this.size = { x: 20, y: 20 };
+		this.color = this.game.randomColors(140);
 		this.speed = 1;
 		this.angularVelocity = this.aim(window.playerPosition);
 
@@ -11,13 +11,16 @@ class Baddies {
 		this.alive = true;
 	}
 
-	draw(size) {
-		screen.clearRect(0, 0, size.x, size.y);
-		for (let body of this.bodies) {
-			this.render(screen, body);
-		}
-	}
+	// draw(size) {
+	// 	screen.clearRect(0, 0, size.x, size.y);
+	// 	for (let body of this.bodies) {
+	// 		this.render(screen, body);
+	// 	}
+	// }
+
 	aim(player) {
+		// Some pretty cool math stuff :P Only reason I knew about this is because of Ryan Dowd
+		// This makes it to where the enemies know where you are and will HUNT YOU DOWN
 		let theta = Math.atan2(player.y - this.position.y, player.x - this.position.x);
 		let xSpeed = this.speed * Math.cos(theta);
 		let ySpeed = this.speed * Math.sin(theta);
