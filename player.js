1165a5553654e477d488e6deaab454fdea0e6316
Charles McKelvey <chazmckelvey@gmail.com>
2019-09-29 21:25:34 -0400

    Moved all of the attack creation to the attack js file. Added boundary collision so you don't just move off the screen

--- player.js
+++ player.js
@@ -2,10 +2,10 @@ class Player {
 	constructor(game, gameSize) {
 		this.game = game;
 		this.position = { x: gameSize.x / 2, y: gameSize.y / 2 }; // This will be updated as keys are pressed
-		this.size = { x: 15, y: 15 }; // Constant dependent upon character type: base character can be 15x15. Warrior 20x20, Mage/Priest base
+		this.size = { x: 20, y: 20 }; // Constant dependent upon character type: base character can be 15x15. Warrior 20x20, Mage/Priest base
 		this.keyboarder = new Keyboarder();
-		this.color = colors.player;
-
+		this.color = this.game.randomColors(100);
+		this.direction = 'DOWN';
 		window.playerPosition = this.position;
 
 		this.collide = true;
@@ -13,60 +13,61 @@ class Player {
 	}
 
 	attack(direction) {
-		if (this.keyboarder.isDown(Keyboarder.KEYS.ATTACK)) {
-			if (direction === 'UP') {
-				// Positioning and sizing and then creating and adding the Attack to the bodies
-				// Supposed to simulate a spear attack.
-				let attackPosition = { x: this.size.x - 8, y: this.size.y - 28 };
-				let attackSize = { width: 15, height: 20 };
-				let attack = new Attack(this.game, attackPosition, attackSize);
-				this.game.addBody(attack);
-			} else if (direction === 'RIGHT') {
-				let attackPosition = { x: this.size.x + 8, y: this.size.y - 7.5 };
-				let attackSize = { x: 20, y: 15 };
-				let attack = new Attack(this.game, attackPosition, attackSize);
-				this.game.addBody(attack);
-			} else if (direction === 'DOWN') {
-				let attackPosition = { x: this.size.x - 7.5, y: this.size.y - 8 };
-				let attackSize = { x: 15, y: 20 };
-				let attack = new Attack(this.game, attackPosition, attackSize);
-				this.game.addBody(attack);
-			} else if (direction === 'LEFT') {
-				let attackPosition = { x: this.size.x - 28, y: this.size.y - 7.5 };
-				let attackSize = { x: 20, y: 15 };
-				let attack = new Attack(this.game, attackPosition, attackSize);
-				this.game.addBody(attack);
-			}
+		if (!this.game.canAttack) {
+			this.game.canAttack = false;
+			let attack = new Attack(this.game, this.position, direction);
+			this.game.addBody(attack);
+			// Should these all be their own separate if statements or else ifs?
 		}
 	}
 
 	update() {
-		let direction = 'DOWN';
 		// Aiming will be apart of the basic attack. You will swing in that direction, creating an invisible rectangle of attack
-		console.log(this.keyboarder);
 		if (this.keyboarder.isDown(Keyboarder.KEYS.MOVELEFT)) {
 			// A
-			this.position.x -= 1.5;
-			direction = 'LEFT';
-			this.attack(direction);
+			this.position.x -= 1.1;
+			this.direction = 'LEFT';
+			// Load sprites for looking left
+			// this.attack(direction);
 		}
 		if (this.keyboarder.isDown(Keyboarder.KEYS.MOVERIGHT)) {
 			// D
-			this.position.x += 1.5;
-			direction = 'RIGHT';
-			this.attack(direction);
+			this.position.x += 1.1;
+			this.direction = 'RIGHT';
+			// Load sprites for looking right
+			// this.attack(direction);
 		}
 		if (this.keyboarder.isDown(Keyboarder.KEYS.MOVEUP)) {
 			// W
-			this.position.y -= 1.5;
-			direction = 'UP';
-			this.attack(direction);
+			this.position.y -= 1.1;
+			this.direction = 'UP';
+			// Load sprites for looking up
+			// this.attack(direction);
 		}
 		if (this.keyboarder.isDown(Keyboarder.KEYS.MOVEDOWN)) {
 			// S
-			this.position.y += 1.5;
-			direction = 'DOWN';
-			this.attack(direction);
+			this.position.y += 1.1;
+			this.direction = 'DOWN';
+			// Load sprites for looking down
+			// this.attack(direction);
+		}
+
+		if (this.keyboarder.isDown(Keyboarder.KEYS.ATTACK)) {
+			this.attack(this.direction);
+			// Attack sprites (Probably 2 frames but one is repeated twice)
+		}
+		// BOUNDARY BLOCKERS
+		if (this.position.x >= this.game.size.x) {
+			this.position.x = this.game.size.x;
+		}
+		if (this.position.y >= this.game.size.y) {
+			this.position.y = this.game.size.y;
+		}
+		if (this.position.x <= 0) {
+			this.position.x = 0;
+		}
+		if (this.position.y <= 0) {
+			this.position.y = 0;
 		}
 	}
 }
