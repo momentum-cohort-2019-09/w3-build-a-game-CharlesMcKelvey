90d5afe69549b955bfb57853f5b697c7cf158be1
Charles McKelvey <chazmckelvey@gmail.com>
2019-09-29 21:22:09 -0400

    Added some constructor ifs to tackle the directions. Changing size and position of the attack dependent upon the direction the player last moved. Seems clunky, may change it to just a box but I like the idea of the spear attack so trying to stick with it

--- attack.js
+++ attack.js
@@ -1,25 +1,43 @@
 class Attack {
-	constructor(game, position, size) {
+	constructor(game, playerPosition, direction) {
 		this.game = game;
-		this.position = { x: position.x, y: position.y };
-		this.size = { width: size.width, height: size.height };
+		if (direction === 'UP') {
+			this.position = { x: playerPosition.x - 8, y: playerPosition.y - 28 };
+			this.size = { x: 15, y: 20 };
+			console.log(this.size); // So this totally exists, it just isn't being drawn.
+		} else if (direction === 'RIGHT') {
+			this.position = { x: playerPosition.x + 8, y: playerPosition.y - 7.5 };
+			this.size = { x: 20, y: 15 };
+		} else if (direction === 'DOWN') {
+			this.position = { x: playerPosition.x - 7.5, y: playerPosition.y - 8 };
+			this.size = { x: 15, y: 20 };
+		} else if (direction === 'LEFT') {
+			this.position = { x: playerPosition.x - 28, y: playerPosition.y - 7.5 };
+			this.size = { x: 20, y: 15 };
+		}
 		this.color = 'rgba(0, 200, 200, 0.5)';
 		this.collide = true;
 		this.cooldown = 0;
 		this.game.attack = this; // This is adding a property to the game that says this is an attack
 	}
 
+	// This should only exist when you are attacking (for that .5 seconds)
+	// which is why the cooldown comes in and works the way it does.
+	// The idea is it comes in and immediately dissappears, even if
+
 	update() {
 		// A tick = 1/60 of a second.
 		// Attack once per 2 seconds : Every 120 ticks
 		// Attack last .5 seconds : 30 Ticks
 		this.cooldown++;
-		if (cooldown === 30) {
+		if (this.cooldown === 30) {
+			clearRect(this.size);
+			// Not sure which way would work better. Elimating size or clearing it away
 			this.size = { x: 0, y: 0 };
-			this.canAttack = false;
-		}
-		if (cooldown === 120) {
-			this.canAttack = true;
+		} else if (this.cooldown === 120) {
+			this.game.bodies = this.game.bodies.filter((body) => body !== this);
+			this.game.canAttack = true;
+			this.cooldown = 0;
 		}
 	}
 }
